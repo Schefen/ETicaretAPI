875933d3720936b1f05411d21930af0921d69e30
Halil_İbrahim_Morgil <SchefenTR@gmail.com>
2023-10-28 03:15:25 +0300

    Optimized Generic Repository

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretAPI.API.Controllers
@@ -7,6 +8,25 @@ namespace ETicaretAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        readonly private IProductWriterRepository _writerRepository;
+        readonly private IProductReadRepository _readRepository;
 
+        public ProductsController(IProductWriterRepository writerRepository, IProductReadRepository readRepository)
+        {
+            _writerRepository = writerRepository;
+            _readRepository = readRepository;
+        }
+        [HttpGet]
+        public async void Get()
+        {
+            await _writerRepository.AddRangeAsync(new()
+            {
+                new() {Id= Guid.NewGuid(),Name="Balta",Price=50,CreatedDate=DateTime.UtcNow,Stock=10},
+                new() {Id= Guid.NewGuid(),Name="Kazma",Price=20,CreatedDate=DateTime.UtcNow,Stock=10},
+                new() {Id= Guid.NewGuid(),Name="Kürek",Price=10,CreatedDate=DateTime.UtcNow,Stock=10},
+                new() {Id= Guid.NewGuid(),Name="Kılıç",Price=40,CreatedDate=DateTime.UtcNow,Stock=10},
+            });
+            var count = await _writerRepository.SaveAsync();
+        }
     }
 }
